3bea5502669f0d42b48a41d4117e710f584b8ac1
everlaime-tech <ever.laime@uab.edu.bo>
2026-07-30 20:00:30 -0600

    modo nocturno primera version

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const cuerpo = document.querySelector("body");
+const botonModo = document.querySelector("#btn-tema");
+
+let esDeDia = false;
+
+function alternarModo()
+{
+    cuerpo.classList.toggle("claro");
+    esDeDia= !esDeDia;
+    if (esDeDia) {
+        botonModo.textContent="Modo noche"
+    } else {
+        botonModo.textContent="Modo noche"
+    }
+}
+botonModo.addEventListener("click", alternarModo);
